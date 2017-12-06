6d146707918b044e2364a69ddb4d360a0a83f828
Phillipp Engelke <zottelchin@gmail.com>
2017-12-06 18:17:52 +0100

    Return error-report to user

--- tcp/server.go
+++ tcp/server.go
@@ -1,39 +1,52 @@
 package qbinTCP
 
 import (
+	"io/ioutil"
 	"net"
+	"strings"
 	"time"
 
 	"github.com/qbin-io/backend"
 )
 
-var froot string
-
 // StartTCP launches the TCP server which is responsible for the TCP API.
 //listen <- given tcp port
 func StartTCP(listen string, root string) {
-	froot = root
+	root = strings.TrimSuffix(root, "/")
 
 	//reading server TCP-Address
-	tcpAddr, err := net.ResolveTCPAddr("tcp4", listen)
+	tcpAddr, err := net.ResolveTCPAddr("tcp", listen)
 	checkErr(err)
 
 	//starting listener on TCP-Address
 	listenerTCP, err := net.ListenTCP("tcp", tcpAddr) //not sure why to use tcpAddr instead of listen
 	checkErr(err)
 
-	qbin.Log.Noticef("TCP server starting on %s, you should be able to reach it at %s", listen, froot)
+	qbin.Log.Noticef("TCP server starting on %s, you should be able to reach it at %s%s", listen, getTCProot(root), listen)
 
 	//waiting for incoming connections
 	for {
 		connTCP, err := listenerTCP.AcceptTCP()
 		if err != nil {
-			qbin.Log.Error("TCP Server Error:", err.Error())
+			qbin.Log.Errorf("TCP Server Error: %s", err.Error())
 			continue
 		}
 
-		go handleClient(connTCP)
+		go handleClient(connTCP, root)
 	}
+}
+
+func getTCProot(root string) string {
+	if strings.Contains(root, "://") {
+		root = root[strings.Index(root, "://")+3:]
+	}
+	for strings.Contains(root, "/") {
+		root = root[0:strings.Index(root, "/")]
+	}
+	if strings.Contains(root, ":") {
+		root = root[0:strings.Index(root, ":")]
+	}
+	return root
 }
 
 //log Error if one occures
@@ -44,37 +57,25 @@ func checkErr(err error) {
 }
 
 //handle incomming TCP Connection
-func handleClient(connTCP net.Conn) {
-	//set timeout to 2 minutes, limit requestlength to 1MB, close connection on programm close
-	connTCP.SetReadDeadline(time.Now().Add(2 * time.Minute))
-	request := make([]byte, 1024)
+func handleClient(connTCP net.Conn, root string) {
+	//set timeout to 5 seconds, limit requestlength to 1MB, close connection on programm close
+	connTCP.SetReadDeadline(time.Now().Add(5 * time.Second))
+	result := make([]byte, 1024)
 	defer connTCP.Close()
 
-	for {
-		msgLength, err := connTCP.Read(request)
-		if err != nil {
-			qbin.Log.Errorf("TCP Read Error: %s", err.Error())
-			break
-		}
+	result, err := ioutil.ReadAll(connTCP)
+	checkErr(err)
+	handleMsgProcessing(connTCP, string(result), root)
 
-		msg := string(request[:msgLength])
-		handleMsgProcessing(connTCP, msg)
-
-		connTCP.Close()
-		break
-	}
 }
 
-func handleMsgProcessing(connTCP net.Conn, msg string) {
+func handleMsgProcessing(connTCP net.Conn, msg string, root string) {
 	//length = 0 -> user already closed connection
 	if len(msg) == 0 {
 		return
 	}
-	//there is a msg, so we process it
-	//sendIP := connTCP.RemoteAddr().String
-	//qbin.Log.Notice("You received something via TCP")
-	//qbin.Log.Notice(msg)
 
+	//there is a msg, so we process it
 	exp, err := qbin.ParseExpiration("14d")
 	checkErr(err)
 
@@ -82,13 +83,17 @@ func handleMsgProcessing(connTCP net.Conn, msg string) {
 		Content:    msg,
 		Syntax:     "",
 		Expiration: exp,
-		Address:    "::ffff:127.0.0.1",
+		Address:    connTCP.RemoteAddr().String(),
 	}
 
 	err = qbin.Store(&doc)
-	checkErr(err) //Todo: User err msg
+	if err != nil {
+		checkErr(err)
+		connTCP.Write([]byte("Ups, something went wrong."))
+		return
+	}
 
-	reply := froot + "/" + doc.ID + "\n"
+	reply := root + "/" + doc.ID + "\n"
 
 	connTCP.Write([]byte(reply))
 }

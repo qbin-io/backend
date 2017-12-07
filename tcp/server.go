87414548598addb4943eae84f10047d687e1c18a
Phillipp Engelke <zottelchin@gmail.com>
2017-12-07 23:52:04 +0100

    Check for service on given port on startup

--- tcp/server.go
+++ tcp/server.go
@@ -15,12 +15,20 @@ func StartTCP(listen string, root string) {
 	root = strings.TrimSuffix(root, "/")
 
 	//reading server TCP-Address
+	qbin.Log.Debug("Resolving TCP...")
 	tcpAddr, err := net.ResolveTCPAddr("tcp", listen)
-	checkErr(err)
+	if err != nil {
+		qbin.Log.Errorf("There was an Error resolving the TCPAddress \n%s\n The Service will be stoped now!", err)
+		return
+	}
 
 	//starting listener on TCP-Address
+	qbin.Log.Debug("Starting TCP-listener")
 	listenerTCP, err := net.ListenTCP("tcp", tcpAddr) //not sure why to use tcpAddr instead of listen
-	checkErr(err)
+	if err != nil {
+		qbin.Log.Errorf("There was an Error starting the TCP-listener \n%s\n The Service will be stoped now!", err)
+		return
+	}
 
 	qbin.Log.Noticef("TCP server starting on %s, you should be able to reach it at %s%s", listen, getTCProot(root), listen)
 
@@ -49,22 +57,20 @@ func getTCProot(root string) string {
 	return root
 }
 
-//log Error if one occures
-func checkErr(err error) {
-	if err != nil {
-		qbin.Log.Errorf("TCP Server Error: %s", err.Error())
-	}
-}
-
 //handle incomming TCP Connection
 func handleClient(connTCP net.Conn, root string) {
+	qbin.Log.Debug("Handeling incomming connection...")
 	//set timeout to 5 seconds, limit requestlength to 1MB, close connection on programm close
-	connTCP.SetReadDeadline(time.Now().Add(5 * time.Second))
+	connTCP.SetReadDeadline(time.Now().Add(1500 * time.Millisecond))
 	result := make([]byte, 1024)
 	defer connTCP.Close()
 
 	result, err := ioutil.ReadAll(connTCP)
-	checkErr(err)
+	if err != nil {
+		qbin.Log.Errorf("TCP-read error: %s", err)
+		connTCP.Write([]byte("Ups, something went wrong."))
+		return
+	}
 	handleMsgProcessing(connTCP, string(result), root)
 
 }
@@ -72,12 +78,15 @@ func handleClient(connTCP net.Conn, root string) {
 func handleMsgProcessing(connTCP net.Conn, msg string, root string) {
 	//length = 0 -> user already closed connection
 	if len(msg) == 0 {
+		qbin.Log.Debug("The TCP message was empty")
 		return
 	}
 
 	//there is a msg, so we process it
 	exp, err := qbin.ParseExpiration("14d")
-	checkErr(err)
+	if err != nil {
+		qbin.Log.Errorf("There was an expiration parsing error: %s", err)
+	}
 
 	doc := qbin.Document{
 		Content:    msg,
@@ -88,7 +97,7 @@ func handleMsgProcessing(connTCP net.Conn, msg string, root string) {
 
 	err = qbin.Store(&doc)
 	if err != nil {
-		checkErr(err)
+		qbin.Log.Errorf("There was an error storing the TCP input: %s", err)
 		connTCP.Write([]byte("Ups, something went wrong."))
 		return
 	}
@@ -96,4 +105,5 @@ func handleMsgProcessing(connTCP net.Conn, msg string, root string) {
 	reply := root + "/" + doc.ID + "\n"
 
 	connTCP.Write([]byte(reply))
+	qbin.Log.Debug("TCP document was saved")
 }

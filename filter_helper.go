2835da794ffb42a33e6197f9501f63c841a998ba
Phillipp Engelke <zottelchin@gmail.com>
2017-12-21 14:37:34 +0100

    Changed searchmethod to regex

--- filter_helper.go
+++ filter_helper.go
@@ -2,6 +2,7 @@ package qbin
 
 import (
 	"errors"
+	"regexp"
 	"strings"
 )
 
@@ -13,15 +14,15 @@ import (
 func FilterSpam(doc *Document) error {
 	Log.Warning("Spamfiltering is still in deveopement.")
 	Log.Debug("Starting spamcheck.")
+
+	//TODO: multiplier by linecount
+
 	lines := strings.Count(doc.Content, "\n")
-	links := 0
-	//check for "http://"
-	links += strings.Count(doc.Content, "http://")
-	//check for "https://"
-	links += strings.Count(doc.Content, "https://")
+	//Count links with regex
+	links := len(regexp.MustCompile("(https://|http://)?(www)?[a-z0-9]*\\.[a-z0-9]*/?").FindAllStringIndex(doc.Content, -1))
 
 	//check if there are too many links:
-	if links*10 >= lines {
+	if links*10 > lines {
 		Log.Error("Content was classified as Spam and will be safed to Spam Table")
 		return errors.New("Internal Server Error")
 	}

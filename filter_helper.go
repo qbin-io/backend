aa813bf8f2b5fa81e567fcaf9b460f97a91894b0
Phillipp Engelke <zottelchin@gmail.com>
2017-12-22 16:53:46 +0100

    Save classified spam to  spam database, if database doesn't exist creating it

--- filter_helper.go
+++ filter_helper.go
@@ -22,9 +22,23 @@ func FilterSpam(doc *Document) error {
 	links := len(regexp.MustCompile("(https://|http://)?(www)?[a-z0-9]*\\.[a-z0-9]*/?").FindAllStringIndex(doc.Content, -1))
 
 	//check if there are too many links:
-	if links*10 > lines {
+	if links*10 >= lines {
 		Log.Error("Content was classified as Spam and will be safed to Spam Table")
+		go saveToSpam(doc)
 		return errors.New("Internal Server Error")
 	}
 	return nil
+}
+
+func saveToSpam(doc *Document) {
+	_, err := db.Exec(
+		"INSERT INTO spam (id, content, upload, address) VALUES (?, ?, ?, ?)",
+		doc.ID,
+		doc.Content,
+		doc.Upload.UTC().Format("2006-01-02 15:04:05"),
+		doc.Address)
+	if err != nil {
+		Log.Errorf("An error occured while saving spam to spam-DB: %s", err)
+	}
+	Log.Debug("Spam was saved to DB.")
 }
